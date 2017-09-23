4a42c8fe40394977b63e40023691f5529820be71
wangqian234 <565059807@qq.com>
2017-09-23 09:27:49 +0800

    改数据库

---
+++ src/com/mvc/controller/sqlConnectionTest.java
@@ -0,0 +1,18 @@
+package com.mvc.controller;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+public class sqlConnectionTest {
+
+	public @ResponseBody String sqlConnectionTest(){
+		List<Object> list;
+		return null;
+		
+	}
+	
+	
+}
